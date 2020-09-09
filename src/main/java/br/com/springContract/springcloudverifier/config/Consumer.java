ba1028c3c5bbbddd67d64ffacbbba193dff129ee
Marcus Jr <marcus.junioor@gmail.com>
2020-09-09 16:58:25 -0300

    ajustes integração kafka

---
+++ src/main/java/br/com/springContract/springcloudverifier/config/Consumer.java
@@ -0,0 +1,21 @@
+package br.com.springContract.springcloudverifier.config;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+class Consumer {
+
+    @KafkaListener(topics = "${cloudkarafka.topic}")
+    public void processMessage(String message,
+                               @Header(KafkaHeaders.RECEIVED_PARTITION_ID) List<Integer> partitions,
+                               @Header(KafkaHeaders.RECEIVED_TOPIC) List<String> topics,
+                               @Header(KafkaHeaders.OFFSET) List<Long> offsets) {
+        System.out.printf("Mensagem consumida do tópico %s-%d[%d] \"%s\"\n", topics.get(0), partitions.get(0), offsets.get(0), message);
+    }
+
+}
--- src/main/java/br/com/springContract/springcloudverifier/controller/ConvidadoController.java
+++ src/main/java/br/com/springContract/springcloudverifier/controller/ConvidadoController.java
@@ -1,14 +1,17 @@
 package br.com.springContract.springcloudverifier.controller;
 
 
+import br.com.springContract.springcloudverifier.config.Producer;
 import br.com.springContract.springcloudverifier.model.Convidado;
 import br.com.springContract.springcloudverifier.service.ConvidadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.websocket.server.PathParam;
 import java.util.List;
 
 @RestController
@@ -17,10 +20,13 @@ public class ConvidadoController {
     @Autowired
     private ConvidadoService convidadoService;
 
-    @Autowired
-    private KafkaTemplate<String, Convidado> kafkaTemplate;
+//    @Autowired
+//    private KafkaTemplate<String, Convidado> kafkaTemplate;
 
-    private static final String TOPIC = "1s0xs9ci-default";
+    @Autowired
+    private Producer producer;
+
+    private static final String TOPIC = "v13jy9uz-default";
 
     public ConvidadoController(ConvidadoService convidadoService) {
         this.convidadoService = convidadoService;
@@ -32,10 +38,16 @@ public class ConvidadoController {
     }
 
 
-    @PostMapping("/publicacao/{nome}")
-    public void publicarConvidado(String nomeConvidado){
-        Convidado convidado = new Convidado(nomeConvidado, "teste@kakfa.com", "1122334455", "12");
-        kafkaTemplate.send(TOPIC, convidado);
+//    @PostMapping("/publicacao/{nome}")
+//    public void publicarConvidado(String nomeConvidado){
+//        Convidado convidado = new Convidado(nomeConvidado, "teste@kakfa.com", "1122334455", "12");
+//        kafkaTemplate.send(TOPIC, convidado);
+//    }
+
+    @PostMapping("/publicar/{mensagem}")
+    public void publicarMensagem(@PathVariable String mensagem){
+        producer.send(mensagem);
     }
+
 
 }
